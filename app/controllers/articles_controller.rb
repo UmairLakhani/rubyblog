2802a29c983fd4d542daf56a6acbff62619b84d1
Umair Lakhani <mairlakhani@gmail.com>
2017-04-24 12:37:39 +0500

    commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
  
-  http_basic_authenticate_with name: "yasir", password: "yasir", except: [:index, :show]
+  http_basic_authenticate_with name: "umair", password: "umair", except: [:index, :show]
  
   def index
     @articles = Article.all
